527aae29b3916d188d01d7949cd7f17bf5b381ce
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-08-10 21:06:33 +0200

    Added astro-robots-txt for robots.txt generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import vue from "@astrojs/vue";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), vue()]
+  site: "https://www.psionicalch.com",
+  integrations: [tailwind(), mdx(), sitemap(), vue(), robotsTxt()],
 });
